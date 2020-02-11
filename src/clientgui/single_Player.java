d527abc94981633b4062ded1fb8c3191c682724d
mohamedco96 <32494528+mohamedco96@users.noreply.github.com>
2020-02-11 18:46:29 +0200

    mohamed

---
+++ src/clientgui/single_Player.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientgui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Mohamed Ibrahim
+ */
+public class single_Player extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent single_Player = FXMLLoader.load(getClass().getResource("single_Player.fxml"));
+        
+        Scene scene = new Scene(single_Player);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/clientgui/single_PlayerController.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientgui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author Mohamed Ibrahim
+ */
+public class single_PlayerController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button bt1;
+    @FXML
+    private Button bt2;
+    @FXML
+    private Button bt3;
+    @FXML
+    private Button bt4;
+    @FXML
+    private Button bt5;
+    @FXML
+    private Button bt6;
+    @FXML
+    private Button bt7;
+    @FXML
+    private Button bt8;
+    @FXML
+    private Button bt9;
+    @FXML
+    private Button exit;
+    @FXML
+    private Pane gameUi;
+    @FXML
+    private Pane userUi;
+    @FXML
+    private Label player1_simple;
+    @FXML
+    private Label player1_score;
+    @FXML
+    private Label player2_simple;
+    @FXML
+    private Label player2_score;
+    @FXML
+    private Label Player1_name;
+    @FXML
+    private Label Player2_name;
+    
+
+
+
+    @FXML
+    private void ButtonAction(ActionEvent event) {
+        if(event.getSource()==exit){
+            System.exit(0);
+        }
+
+//         gameUi.toFront();
+//            userUi.toFront();
+        if(event.getSource()==bt1){
+            bt1.setText("X");   //For test
+            Player1_name.setText("Mohamed");    //For test
+        }
+        
+        if(event.getSource()==bt2){
+        }
+        
+        if(event.getSource()==bt3){
+        }
+        
+        if(event.getSource()==bt4){
+        }
+        
+        if(event.getSource()==bt5){
+        }
+        
+        if(event.getSource()==bt6){
+        }
+        
+        if(event.getSource()==bt7){
+        }
+        
+        if(event.getSource()==bt8){
+        }
+        
+        if(event.getSource()==bt9){
+        }
+        
+        
+       
+
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
